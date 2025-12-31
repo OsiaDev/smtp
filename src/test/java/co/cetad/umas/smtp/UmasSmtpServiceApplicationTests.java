10c295432831b9f8e03044586a6a41dc7393e036
Cristian Osia <osia.developer@gmail.com>
2025-12-31 17:45:14 -0500

    Init

---
+++ src/test/java/co/cetad/umas/smtp/UmasSmtpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.cetad.umas.smtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmasSmtpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
